52678db3d3732532331470aa20f1e277b2193f6a
kartik-dash <kartikdash473@gmail.com>
2025-05-06 09:25:50 +0530

    api

--- utkalSanskriti_frontend/src/redux/api/api.jsx
+++ utkalSanskriti_frontend/src/redux/api/api.jsx
@@ -9,7 +9,7 @@
 // const api = axios.create({
 //   baseURL: API_URL,
 //   headers: {
-//     'Content-Type': 'application/json',
+//     'Content-Type': 'application/json',llllllllllll
 //   },
 // });
 
